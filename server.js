7f355889685630e4dc32a0f802563c03a7cbbe92
gcnaveen <naveengc99@gmail.com>
2023-01-23 23:25:58 +0530

    final code

--- server.js
+++ server.js
@@ -36,6 +36,10 @@ app.get('/api/keys/google', (req, res) => {
 
 const __dirname = path.resolve();
 
+app.use(function (req, res) {
+  res.header('Access-Control-Allow-Origin', '*');
+});
+
 app.use(express.static(path.join(__dirname, '/frontend/build')));
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
